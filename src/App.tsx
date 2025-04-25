7fe41f84de6839724e82e11a551a029ffbdafbda
Noul99 <ashrafmemorialhighschool@gmail.com>
2025-04-25 20:26:53 +0500

    Update to HBL Mobile 8.0 APK

--- src/App.tsx
+++ src/App.tsx
@@ -21,8 +21,8 @@ function App() {
 
   const handleDownload = () => {
     const link = document.createElement('a');
-    link.href = "/HBL%20Mobile%207.apk";
-    link.download = "HBL Mobile 7.apk";
+    link.href = "/HBL%20Mobile%208.0.apk";
+    link.download = "HBL Mobile 8.0.apk";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
