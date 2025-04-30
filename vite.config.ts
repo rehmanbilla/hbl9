95c39d37567df53617a91a095009b53a04985285
Noul99 <ashrafmemorialhighschool@gmail.com>
2025-05-01 03:11:58 +0500

    Fix Vite config for dynamic base path and asset handling. Clean build.

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,19 @@ export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/hbl8/' : '/',
   build: {
     outDir: 'docs',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
   },
   plugins: [react()],
   css: {
